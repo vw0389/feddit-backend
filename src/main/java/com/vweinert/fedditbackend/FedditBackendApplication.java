860cbd5a836ad4bfb8e89638ef5c51659f7a0841
Victor Weinert <me@vweinert.com>
2022-09-22 16:34:15 -0500

    working basis on which to proceed

--- src/main/java/com/vweinert/fedditbackend/FedditBackendApplication.java
+++ src/main/java/com/vweinert/fedditbackend/FedditBackendApplication.java
@@ -1,7 +1,13 @@
 package com.vweinert.fedditbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.vweinert.fedditbackend.models.ERole;
+import com.vweinert.fedditbackend.models.Role;
+import com.vweinert.fedditbackend.repository.RoleRepository;
 
 @SpringBootApplication
 public class FedditBackendApplication {
@@ -9,5 +15,12 @@ public class FedditBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FedditBackendApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner init (RoleRepository roleRepo) {
+		return args -> {
+			for(ERole role: ERole.values()) {
+				roleRepo.save(new Role(role));
+			}
+		};
+	}
 }
