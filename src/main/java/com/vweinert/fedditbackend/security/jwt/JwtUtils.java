cd98ed403591f2437392d5cc71b017fb93757916
Victor Weinert <me@vweinert.com>
2022-12-19 21:13:24 -0600

    working on getting private and public key inserted into the component

--- src/main/java/com/vweinert/fedditbackend/security/jwt/JwtUtils.java
+++ src/main/java/com/vweinert/fedditbackend/security/jwt/JwtUtils.java
@@ -1,58 +1,66 @@
+/* (C)2022 */
 package com.vweinert.fedditbackend.security.jwt;
 
+import com.vweinert.fedditbackend.security.services.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import com.vweinert.fedditbackend.security.services.UserDetailsImpl;
-import io.jsonwebtoken.*;
-
 @Component
 public class JwtUtils {
-  private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-  @Value("${feddit.app.jwtSecret}")
-  private String jwtSecret;
+    @Value("${feddit.app.jwtSecret}")
+    private String jwtSecret;
 
-  @Value("${feddit.app.jwtExpirationMs}")
-  private int jwtExpirationMs;
+    private JwtParser parser = Jwts.parserBuilder().setSigningKey().build();
 
-  public String generateJwtToken(Authentication authentication) {
+    @Value("${feddit.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
 
-    UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+    private static void setupRSA(String secret) {
+      
+    }
+    public String generateJwtToken(Authentication authentication) {
 
-    return Jwts.builder()
-        .setSubject((userPrincipal.getUsername()))
-        .setIssuedAt(new Date())
-        .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-        .signWith(SignatureAlgorithm.HS512, jwtSecret)
-        .compact();
-  }
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
-  public String getUserNameFromJwtToken(String token) {
-    return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
-  }
-
-  public boolean validateJwtToken(String authToken) {
-    try {
-      Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
-      return true;
-    } catch (SignatureException e) {
-      logger.error("Invalid JWT signature: {}", e.getMessage());
-    } catch (MalformedJwtException e) {
-      logger.error("Invalid JWT token: {}", e.getMessage());
-    } catch (ExpiredJwtException e) {
-      logger.error("JWT token is expired: {}", e.getMessage());
-    } catch (UnsupportedJwtException e) {
-      logger.error("JWT token is unsupported: {}", e.getMessage());
-    } catch (IllegalArgumentException e) {
-      logger.error("JWT claims string is empty: {}", e.getMessage());
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.RS512, )
+                // .compact();
     }
 
-    return false;
-  }
+    public String getUserNameFromJwtToken(String token) {
+        return parser.parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            parser.parseClaimsJws(authToken);
+            return true;
+        } catch (SecurityException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
 }
