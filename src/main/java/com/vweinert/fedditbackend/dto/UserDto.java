3e1a8ba31a72f8a675f997d71889ace8ce5b023b
Victor Weinert <me@vweinert.com>
2022-12-29 16:26:58 -0600

    added user roles to DTO

--- src/main/java/com/vweinert/fedditbackend/dto/UserDto.java
+++ src/main/java/com/vweinert/fedditbackend/dto/UserDto.java
@@ -1,11 +1,13 @@
 package com.vweinert.fedditbackend.dto;
 
+import com.vweinert.fedditbackend.entities.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Data
 @Builder
@@ -17,5 +19,5 @@ public class UserDto {
     private String about;
     private LocalDateTime createdAt;
     private String jwt;
-
+    private Set<Role> roles;
 }
