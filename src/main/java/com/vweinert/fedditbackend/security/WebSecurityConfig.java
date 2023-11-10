d81ffbb1390cf4c79c0ff1e45ab217f898d10cab
Victor Weinert <me@vweinert.com>
2023-11-10 05:59:12 -0600

    commit

--- src/main/java/com/vweinert/fedditbackend/security/WebSecurityConfig.java
+++ src/main/java/com/vweinert/fedditbackend/security/WebSecurityConfig.java
@@ -60,7 +60,7 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.cors(Customizer.withDefaults());
-    http.csrf(AbstractHttpConfigurer::disable);
+    http.csrf(Customizer.withDefaults());
     http.exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer.authenticationEntryPoint(unauthorizedHandler));
     http.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     http.authorizeHttpRequests(
