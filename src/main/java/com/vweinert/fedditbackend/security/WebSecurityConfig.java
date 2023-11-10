179215de9bdb63f3c2e0a5d91bde77f873cd9f6e
Victor Weinert <me@vweinert.com>
2023-11-10 05:54:22 -0600

    refactored app, remade filterchain to use lambdas instead of depreciated functions

--- src/main/java/com/vweinert/fedditbackend/security/WebSecurityConfig.java
+++ src/main/java/com/vweinert/fedditbackend/security/WebSecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -56,16 +59,16 @@ public class WebSecurityConfig {
   
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable()
-        .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-            .authorizeHttpRequests((requests) -> requests
+    http.cors(Customizer.withDefaults());
+    http.csrf(AbstractHttpConfigurer::disable);
+    http.exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer.authenticationEntryPoint(unauthorizedHandler));
+    http.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    http.authorizeHttpRequests(
+            (requests) -> requests
                     .requestMatchers("/api/auth/**").permitAll()
                     .requestMatchers("/api/home/**").permitAll()
-                    .requestMatchers("/actuator/**").permitAll()
                     .requestMatchers("/error").permitAll()
-                    .anyRequest().authenticated()
-            );
+                    .anyRequest().authenticated());
     http.authenticationProvider(authenticationProvider());
     http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     return http.build();
